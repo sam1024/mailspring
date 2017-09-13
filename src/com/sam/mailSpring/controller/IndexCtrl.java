dc0d0a2d79cd9c1e964ad341ba6458c03129ee24
Unknown <sam.abundis82@gmail.com>
2017-09-12 22:54:33 -0500

    cambio en configuracion
    Cambie una dependencia de pom.xml y agregue unas lineas al Dispatcher-servlet.xml

--- src/com/sam/mailSpring/controller/IndexCtrl.java
+++ src/com/sam/mailSpring/controller/IndexCtrl.java
@@ -1,21 +1,22 @@
 package com.sam.mailSpring.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class IndexCtrl {
 	
+	@Autowired
 	private JavaMailSender java_mail_sender;
-	private SimpleMailMessage simple_mail_message;
+	//private SimpleMailMessage simple_mail_message;
 	
 	@RequestMapping("/")
 	public String showIndex() {
@@ -23,15 +24,18 @@ public class IndexCtrl {
 	}
 	
 	@RequestMapping(value = "/send", method = RequestMethod.POST)
-//	public String send(@ModelAttribute("to") String to, @ModelAttribute("asunto") String asunto,
-//			@ModelAttribute("msj") String msj, Model model) {
 	public String send(HttpServletRequest r) {
-		simple_mail_message = new SimpleMailMessage();
+		SimpleMailMessage simple_mail_message = new SimpleMailMessage();
+		simple_mail_message.setFrom("reservaciones@loyola.edu.mx");
+		simple_mail_message.setReplyTo("nada");
+		simple_mail_message.setSentDate(new Date());
 		simple_mail_message.setTo(r.getParameter("to"));
 		simple_mail_message.setSubject(r.getParameter("asunto"));
 		simple_mail_message.setText(r.getParameter("msj"));
+		
 		System.out.println("Para: " + r.getParameter("to") + "\nAsunto: " + r.getParameter("asunto") + "\nMensaje: " + r.getParameter("msj"));
 		try {
+			System.out.println("SIMPLE MAIL MESSAGE2: " + simple_mail_message);
 			java_mail_sender.send(simple_mail_message);
 		} catch (Exception e) {
 			System.out.println("AL INTENTAR MANDAR EL CORREO SURGIO EL ERROR: " + e);
